c25f726df7ba71f90231b669acdf9e3976b634f5
Harish Janardhanan <harishjan@gmail.com>
2022-02-07 18:45:02 -0500

    remove lock file

--- webpack.production.js
+++
@@ -1,8 +0,0 @@
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
-module.exports = {
-   output: {
-      filename: 'bundle.prod.js' //[1]
-   },
-    plugins: [new CleanWebpackPlugin()], //[2]
-};
